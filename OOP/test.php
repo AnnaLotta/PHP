ade20c6ec1d59f4e59e7cab5fefd4c935719822a
AnnaLotta <anna-lotta.kaukes@khk.ee>
2017-01-11 15:14:39 +0200

    added constructor to text class.

--- OOP/test.php
+++ OOP/test.php
@@ -12,11 +12,21 @@ $sentence = new text();
 echo '<pre>';
 print_r($sentence);
 echo '</pre>';
+// set text
 $sentence->setText('Hello text object!');
 echo '<pre>';
 print_r($sentence);
 echo '</pre>';
+// show object output
 $sentence->show();
+echo '<hr/>';
+// create an object
+// sentence 2
+$sentence2 = new text('Hello text by construct.');
+// control object output
+echo '<pre>';
+print_r($sentence2);
+echo '</pre>'
 
 
 ?>
