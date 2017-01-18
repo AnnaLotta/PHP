ec2e6593465704c901d0afd384029fa350b1114f
AnnaLotta <anna-lotta.kaukes@khk.ee>
2017-01-18 08:51:01 +0200

    catching up

--- 120117/classes/template.php
+++ 120117/classes/template.php
@@ -6,31 +6,44 @@
  * Date: 1/12/2017
  * Time: 12:27 PM
  */
-// if TMPL-DIR is not defined
-if(!define('TMPL_DIR')){
-    // define TMPL-DIR
+// if TMPL_DIR is not defined
+if(!defined('TMPL_DIR')){
+    // define this constant and use in class template
     define('TMPL_DIR', '../tmpl/');
 }
-
 class template
 {
-    var $file = ''; //template filename
-    var $content = false; // template content is empty
-
+    // class variables
+    var $file = ''; // template file name
+    var $content = false; // template content - now is empty
     // class methods
     function loadFile(){
-        $f = $this->file;
-        if(is_dir(TMPL_DIR)){
-            echo 'Kataloogi '.TMPL_DIR.' ei ole leitud./>';
+        $f = $this->file; // use file name variable
+        // if some problem with tmpl directory
+        if(!is_dir(TMPL_DIR)){
+            echo 'Kataloogi '.TMPL_DIR.' ei ole leitud<br/>';
             exit;
         }
-            if (file_exists($f) and is_file($f) and is_readable($f)){
-                $this->readFile($f);
-            }
-            //we can set path to template files ../tmpl/file.html
-    } // loadFile
-
-    function readFile($f) {
+        // if we already in tmpl directory - $this->file is 'tmpl/file.html'
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        // we can set path to template file: tmpl/file.html, $this->file is file.html
+        $f = TMPL_DIR.$this->file;
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        // we can set only file name, $this->file is file
+        $f = TMPL_DIR.$this->file.'.html';
+        if(file_exists($f) and is_file($f) and is_readable($f)){
+            $this->readFile($f);
+        }
+        if($this->content === false){
+            echo 'Ei saanud lugeda faili '.$this->file.'.<br/>';
+            exit;
+        }
+    }// loadFile
+    function readFile($f){
         $this->content = file_get_contents($f);
-    } // readFile
-} // class end
+    }// readFile
+}// class end}
---
+++ 120117/index.php
@@ -0,0 +1,55 @@
+<?php
+// index.php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 12.01.2017
+ * Time: 12:58
+ */
+// create and template object
+define('CLASSES_DIR', 'classes/');
+define('TMPL_DIR', 'tmpl/');
+define('STYLE_DIR', 'css/');
+require_once CLASSES_DIR.'template.php';
+// and use it
+// create an template object,
+// set up the file name for template
+// load template file content
+$tmpl = new template('main');
+// add pairs of temlate element names and real values
+$tmpl->set('style', STYLE_DIR.'main'.'.css');
+$tmpl->set('header', 'minu lehe pealkiri');
+$tmpl->set('menu', 'minu menüü');
+$tmpl->set('nav_bar', 'minu navigatsioon');
+$tmpl->set('lang_bar', 'minu keeleriba');
+$tmpl->set('content', 'minu sisu');
+/*
+// control the content of template object
+echo '<pre>';
+print_r($tmpl);
+echo '</pre>';
+*/
+// output template content set up with real values
+echo $tmpl->parse();
+// import http class
+require_once CLASSES_DIR.'http.php';
+// create and output http object
+$http = new http();
+// control http object output
+echo '<pre>';
+print_r($http);
+echo '</pre>';
+// control http constants
+echo REMOTE_ADDR.'<br />';
+echo PHP_SELF.'<br />';
+echo SCRIPT_NAME.'<br />';
+echo HTTP_HOST.'<br />';
+echo '<hr />';
+// create http data pairs and set up into $http->vars array
+$http->set('kasutaja', 'Anna');
+$http->set('tund', 'php programmeerimisvahendid');
+// control $http->vars object output
+echo '<pre>';
+print_r($http->vars);
+echo '</pre>';
+?>
