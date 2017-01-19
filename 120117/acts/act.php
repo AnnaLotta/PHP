2cfc068321e2c50c83238e59422467bc13b61c8c
Ari <anna-lotta.kaukes@khk.ee>
2017-01-19 12:36:51 +0200

    catching up

---
+++ 120117/acts/act.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: anna.karutina
+ * Date: 18.01.2017
+ * Time: 14:25
+ */
+// get act element value from url
+$act = $http->get('act');
+// define act file path according to the act element value
+$fn = ACTS_DIR.str_replace('.', '/', $act).'.php';
+// control act file
+if(file_exists($fn) and is_file($fn) and is_readable($fn)){
+    // import act file
+    require_once $fn;
+} else {
+    // define default act file path
+    $fn = ACTS_DIR.DEFAULT_ACT.'.php';
+    // define new value for act element in url
+    $http->set('act', DEFAULT_ACT);
+    // use default act
+    require_once $fn;
+}
+?>
--- 120117/index.php
+++ 120117/index.php
@@ -6,12 +6,8 @@
  * Date: 12.01.2017
  * Time: 12:58
  */
-// create and template object
-define('CLASSES_DIR', 'classes/');
-define('TMPL_DIR', 'tmpl/');
-define('STYLE_DIR', 'css/');
-require_once CLASSES_DIR.'template.php';
-// and use it
+// import configuration
+require_once 'conf.php';
 // create an template object,
 // set up the file name for template
 // load template file content
@@ -19,37 +15,16 @@ $tmpl = new template('main');
 // add pairs of temlate element names and real values
 $tmpl->set('style', STYLE_DIR.'main'.'.css');
 $tmpl->set('header', 'minu lehe pealkiri');
-$tmpl->set('menu', 'minu menüü');
+// create and output menu
+// import menu file
+require_once 'menu.php'; // in this file is menu creation
+$tmpl->set('menu', $menu->parse());
 $tmpl->set('nav_bar', 'minu navigatsioon');
 $tmpl->set('lang_bar', 'minu keeleriba');
 $tmpl->set('content', 'minu sisu');
-/*
-// control the content of template object
-echo '<pre>';
-print_r($tmpl);
-echo '</pre>';
-*/
 // output template content set up with real values
 echo $tmpl->parse();
-// import http class
-require_once CLASSES_DIR.'http.php';
-// create and output http object
-$http = new http();
-// control http object output
-echo '<pre>';
-print_r($http);
-echo '</pre>';
-// control http constants
-echo REMOTE_ADDR.'<br />';
-echo PHP_SELF.'<br />';
-echo SCRIPT_NAME.'<br />';
-echo HTTP_HOST.'<br />';
-echo '<hr />';
-// create http data pairs and set up into $http->vars array
-$http->set('kasutaja', 'Anna');
-$http->set('tund', 'php programmeerimisvahendid');
-// control $http->vars object output
-echo '<pre>';
-print_r($http->vars);
-echo '</pre>';
+// control actions
+// import act file
+require_once 'act.php';
 ?>
